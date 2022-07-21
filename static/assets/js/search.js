44b690ab7aecb37c35913f1770aaf33b9ece6f62
Al Asad Nur Riyad <asadnurriyad@appscode.com>
2022-07-21 16:09:21 +0600

    Add Search options
    Signed-off-by: Al Asad Nur Riyad <asadnurriyad@appscode.com>

--- static/assets/js/search.js
+++ static/assets/js/search.js
@@ -1,9 +1,11 @@
 const keywords =  new Set();
+let searchKeyword = "";
 
 const kubedb = document.getElementById("kubedb");
 const stash = document.getElementById("stash");
 const kubevault = document.getElementById("kubevault");
 const kubeform = document.getElementById("kubeform");
+const searchElement = document.getElementById("search");
 
 kubedb.addEventListener("change",(Event)=>{
   let isChecked = Event.target.checked; 
@@ -33,23 +35,43 @@ kubeform.addEventListener("change",(Event)=>{
   filterList();
 })
 
+searchElement.addEventListener("input",(event)=>{
+  let str = searchElement.value;
+  searchKeyword = str.toLowerCase();
+  filterList();
+})
 
+//Filter based on the tags and search keyword
 const filterList = () =>{
   const cards = document.getElementById("card-list");
   const cardList = cards.querySelectorAll(".column");
   cardList.forEach(card => {
-    const tags = card.querySelector(".tags").innerText;
-    if(!isTagAvailable(tags.toLowerCase())){
+    const tags = card.querySelector(".tags").innerText.toLowerCase();
+    const author = card.querySelector(".author").innerText.toLowerCase();
+    const heading = card.querySelector("h2").innerText.toLowerCase();
+    if(!isTagAvailable(tags,author,heading)){
       card.style.display = "none";
     }else card.style.display = "block"
   })
 }
 
-const isTagAvailable = (tags) =>{
-  if(keywords.size === 0) return true;
+//Check if tags & search keyword contains in cards tags, auther and heading
+const isTagAvailable = (tags,author,heading) =>{
+  if(keywords.size === 0 && searchKeyword.length<3) return true;
   let flag = false;
-  keywords.forEach(key=>{
-    flag |= tags.includes(key);
-  })
-  return flag;
+  if(keywords.size === 0){
+    flag |= tags.includes(searchKeyword);
+    flag |= author.includes(searchKeyword);
+    flag |= heading.includes(searchKeyword);
+  }else if(searchKeyword.length<3){
+    keywords.forEach(key=>{
+      flag |= tags.includes(key);
+    })
+  }else{
+    keywords.forEach(key=>{
+      let flag1 = tags.includes(key) 
+      let flag2 = tags.includes(searchKeyword) || author.includes(searchKeyword) || heading.includes(searchKeyword);
+      flag = flag1 & flag2;
+    })
+  }return flag;
 }
