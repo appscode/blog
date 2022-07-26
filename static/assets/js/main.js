947cb9f31e863c94107ef3337d4ec54e74bfdc26
Md Mohin Uddin <mohin@appscode.com>
2022-07-26 17:04:48 +0600

    Fix various responsive issues
    Signed-off-by: Md Mohin Uddin <mohin@appscode.com>

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -4,26 +4,15 @@ document.addEventListener("DOMContentLoaded", () => {
     once: true,
   });
 
-  // Get all "navbar-burger" elements
-  const $navbarBurgers = Array.prototype.slice.call(
-    document.querySelectorAll(".navbar-burger"),
-    0
-  );
-  // Check if there are any navbar burgers
-  if ($navbarBurgers.length > 0) {
-    // Add a click event on each of them
-    $navbarBurgers.forEach((el) => {
-      el.addEventListener("click", () => {
-        // Get the target from the "data-target" attribute
-        const target = el.dataset.target;
-        const $target = document.getElementById(target);
-
-        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-        el.classList.toggle("is-active");
-        $target.classList.toggle("is-active");
-      });
-    });
-  }
+  // navbar for mobile device
+  let navbar = document.querySelector(".navbar-burger");
+  navbar?.addEventListener("click", function () {
+    const hasActiveClass = navbar.classList.contains("is-active");
+    let dropdown = document.querySelector(".navbar-right");
+    navbar.classList.toggle("is-active");
+    dropdown.style.opacity = 1 - dropdown.style.opacity;
+    dropdown.style.visibility = hasActiveClass ? "hidden" : "visible";
+  });
 
   // blog page hero area slider start
   var sliderElement = document.getElementById("hero-area-blog");
@@ -46,7 +35,7 @@ document.addEventListener("DOMContentLoaded", () => {
     },
     dragEnd: () => {
       autoplay(true);
-    }
+    },
   });
 
   sliderElement?.addEventListener("mouseover", () => {
